e721af866f198dfa81eb1bbda4053668b5388818
Maximiliam999 <maximanuel777@gmail.com>
2023-01-09 17:11:39 +0000

    Add event listeners for documnet loading and vutton clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+// Wait for the dom to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
 function runGame() {
 
 }
@@ -10,18 +28,22 @@ function calculateCorrectAnswer() {
 
 }
 
-function incrementWrongAnswer(){
+function incrementScore() {
 
 }
 
-function displayAdditionQuestion(){
+function incrementWrongAnswer() {
 
 }
 
-function displaySubtractionQuestion(){
+function displayAdditionQuestion() {
 
 }
 
-function displayMultiplyQuestion(){
+function displaySubtractQuestion() {
 
+}
+
+function displayMultiplyQuestion() {
+    
 }
